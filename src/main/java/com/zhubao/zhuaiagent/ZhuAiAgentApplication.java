0b969752c8fce41d055bfef43bb4a3d3058d0e6f
huangdazhu <huangdazhu3@qq.com>
2026-06-09 19:39:07 +0800

    项目init

---
+++ src/main/java/com/zhubao/zhuaiagent/ZhuAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.zhubao.zhuaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhuAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhuAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhubao/zhuaiagent/ZhuAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhubao.zhuaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhuAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
